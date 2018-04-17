2335b6a25a6017fead3f9296ea66485d0f9b90b2
서윤호 <seoyunho@seoyunhoui-MacBook-Pro.local>
2018-04-17 10:06:05 +0900

    [CREATE] 프로젝트 구조 생성

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import './index.css';
+import { Main } from './container/index';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Main/>
+  , document.getElementById('root'));
