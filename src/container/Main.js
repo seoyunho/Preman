31cef31a035df049c414cb822cd0f3b7b8e16e12
seoyunho <yunho101775@gmail.com>
2018-05-29 16:03:20 +0900

    main container 생성

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import './css/main.css';
 
 class Main extends Component {
   render() {
     return (
-      <div></div>
+      <React.Fragment>
+        <div id="header-section">
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+        <div id="main-section">
+          <div>
+            
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
